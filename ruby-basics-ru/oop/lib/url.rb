f347723c029ecc1c4aa08057aa2e8993c35d3cc6
@hexlet/cli <support@hexlet.io>
2023-10-19 04:53:31 +0700

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -5,38 +5,28 @@ require 'uri'
 require 'forwardable'
 
 class Url
+  include Comparable
   extend Forwardable
 
-  include Comparable
+  def_delegators :@uri, :scheme, :host, :port
 
   attr_reader :query_params
 
   def initialize(text)
     @uri = URI(text)
-    @query_params =
-      (@uri.query || '').split('&')
-                        .sort
-                        .to_h { |key_value_text| key_value_text.split('=') }
-                        .transform_keys(&:to_sym)
+    @query_params = (@uri.query || '')
+                    .split('&')
+                    .sort
+                    .to_h { |key_value_text| key_value_text.split('=') }
+                    .transform_keys(&:to_sym)
   end
 
-  def_delegators :@uri, :scheme, :host, :port
-
   def query_param(key, default_value = nil)
-    query_params.key?(key) ? query_params[key] : default_value
+    query_params.fetch(key, default_value)
   end
 
   def <=>(other)
-    result = scheme <=> other.scheme
-    return result unless result.zero?
-
-    result = host <=> other.host
-    return result unless result.zero?
-
-    result = port <=> other.port
-    return result unless result.zero?
-
-    query_params <=> other.query_params
+    [scheme, host, port, query_params] <=> [other.scheme, other.host, other.port, other.query_params]
   end
 end
 # END
