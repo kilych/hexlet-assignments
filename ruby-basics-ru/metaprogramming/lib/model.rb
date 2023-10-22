cb61bbfde7d6f6cbf1e2bf85b07c91c76a314d02
@hexlet/cli <support@hexlet.io>
2023-10-22 22:57:18 +0700

    submit ruby-basics-ru/metaprogramming

--- ruby-basics-ru/metaprogramming/lib/model.rb
+++ ruby-basics-ru/metaprogramming/lib/model.rb
@@ -4,17 +4,12 @@
 require 'date'
 
 module Model
-  def self.included(base)
-    base.extend(ClassMethods)
-    base.class_variable_set '@@attrs', {}
-  end
+  def initialize(attrs = {})
+    attr_opts = self.class.attribute_options
+    attrs.slice(*attr_opts.keys).each do |attr, val|
+      options = attr_opts[attr]
 
-  def initialize(attrs_to_vals = {})
-    attrs = self.class.class_variable_get '@@attrs'
-    attrs_to_vals.slice(*attrs.keys).each do |attr, val|
-      options = attrs[attr]
-
-      val = convert_to(val, options[:type]) if options.key?(:type)
+      val = self.class.convert_to(val, options[:type]) if options.key?(:type)
 
       instance_variable_set "@#{attr}", val
     end
@@ -22,33 +17,21 @@ module Model
 
   def attributes
     self.class
-        .class_variable_get('@@attrs')
+        .attribute_options
         .keys
         .each_with_object({}) do |attr_name, attrs|
           attrs[attr_name] = send attr_name
         end
   end
 
-  def convert_to(val, type)
-    if !val.nil? && type == :datetime
-      DateTime.parse(val)
-    else
-      # Kernel.const_get(type.to_s.capitalize).new(val)
-
-      conversion_method = "to_#{type}"
-      if val.respond_to?(conversion_method)
-        val.send(conversion_method)
-      else
-        val
-      end
-    end
-  end
-
   module ClassMethods
+    def attribute_options
+      @attribute_options || {}
+    end
+
     def attribute(name, **options)
-      old_attrs = class_variable_get('@@attrs')
-      new_attrs = old_attrs.merge({ name => options })
-      class_variable_set '@@attrs', new_attrs
+      @attribute_options ||= {}
+      @attribute_options[name] = options
 
       define_method name do
         if instance_variable_defined? "@#{name}"
@@ -59,10 +42,29 @@ module Model
       end
 
       define_method "#{name}=" do |val|
-        val = convert_to(val, options[:type]) if options.key?(:type)
+        val = self.class.convert_to(val, options[:type]) if options.key?(:type)
         instance_variable_set "@#{name}", val
       end
     end
+
+    def convert_to(val, type)
+      if !val.nil? && type == :datetime
+        DateTime.parse(val)
+      else
+        # Kernel.const_get(type.to_s.capitalize).new(val)
+
+        conversion_method = "to_#{type}"
+        if val.respond_to?(conversion_method)
+          val.send(conversion_method)
+        else
+          val
+        end
+      end
+    end
+  end
+
+  def self.included(base)
+    base.extend(ClassMethods)
   end
 end
 # END
