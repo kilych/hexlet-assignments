27cbdc0ffef0a28817543e7e0963104a48b3fc0a
@hexlet/cli <support@hexlet.io>
2023-10-29 23:54:20 +0700

    submit rails-basics-ru/rack

---
+++ rails-basics-ru/rack/lib/app/execution_timer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ExecutionTimer
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+    status, headers, body = @app.call(env)
+
+    finish = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    elapsed = finish - start
+    body << "#{elapsed} seconds"
+
+    [status, headers, body]
+  end
+end
