430382693cfd33839bb552bb6211c48388922418
@hexlet/cli <support@hexlet.io>
2023-10-31 16:48:26 +0700

    submit rails-basics-ru/static-pages

---
+++ rails-basics-ru/static-pages/app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# root controller
+class HomeController < ApplicationController
+  def index; end
+end
---
+++ rails-basics-ru/static-pages/app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# pages path prefix controller
+class PagesController < ApplicationController
+  def show; end
+end
